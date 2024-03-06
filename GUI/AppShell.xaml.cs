ad30d17db4d4391ef5d6b6ee563829fc388d62b2
md8n <lee@md8n.com>
2024-03-06 13:33:12 +0900

    Added Split command

--- GUI/AppShell.xaml.cs
+++ GUI/AppShell.xaml.cs
@@ -5,7 +5,7 @@ public partial class AppShell : Shell {
         InitializeComponent();
 
         Routing.RegisterRoute(nameof(Views.CleanPage), typeof(Views.CleanPage));
-        //Routing.RegisterRoute(nameof(Views.SplitPage), typeof(Views.SplitPage));
+        Routing.RegisterRoute(nameof(Views.SplitPage), typeof(Views.SplitPage));
         //Routing.RegisterRoute(nameof(Views.MergePage), typeof(Views.MergePage));
         Routing.RegisterRoute(nameof(Views.AboutPage), typeof(Views.AboutPage));
     }
---
+++ GUI/ViewModels/SplitViewModel.cs
@@ -0,0 +1,89 @@
+﻿using Actions.Split;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using GUI.Models;
+
+namespace GUI.ViewModels;
+
+internal class SplitViewModel : ObservableObject {
+    private Split _split;
+    private string[] _logging;
+    private readonly object _loggingLock = new object();
+
+    public string Source {
+        get => _split.Filename;
+    }
+
+    public string Logging {
+        get => string.Join(Environment.NewLine, _logging);
+        set {
+            lock (_loggingLock) {
+                if (string.IsNullOrWhiteSpace(value)) {
+                    _logging = [];
+                } else {
+                    _logging = _logging.Where(l => l != value).Append(value).ToArray();
+                }
+            }
+        }
+    }
+
+    public AsyncRelayCommand SelectCommand { get; private set; }
+    public AsyncRelayCommand SplitCommand { get; private set; }
+
+    public SplitViewModel() {
+        _split = new Split {
+            Filename = ""
+        };
+        _logging = [];
+        SelectCommand = new AsyncRelayCommand(Select);
+        SplitCommand = new AsyncRelayCommand(Split, CanSplit);
+    }
+
+    private async Task Select() {
+        var file = await FilePicker.Default.PickAsync(new PickOptions {
+            PickerTitle = "Select GCode File"
+        });
+        if (file == null) {
+            RefreshProperties();
+            return;
+        }
+        var fileSource = file.FullPath;
+        if (string.IsNullOrWhiteSpace(fileSource)) {
+            RefreshProperties();
+            return;
+        }
+        _split = Models.Split.Load(fileSource);
+        RefreshProperties();
+    }
+
+    private bool CanSplit() {
+        return !string.IsNullOrWhiteSpace(_split.Filename);
+    }
+
+    private void DoLogging(string logMessage) {
+        Logging = logMessage;
+        OnPropertyChanged(nameof(Logging));
+    }
+
+    private async Task Split() {
+        string lastMessage = "";
+        DoLogging("Starting");
+
+        await Task.Run(async () => {
+            await foreach (string logMessage in SplitAction.ExecuteAsync(new FileInfo(_split.Filename))) {
+                DoLogging(logMessage);
+                lastMessage = logMessage;
+            }
+        });
+
+        var result = lastMessage == "Success" ? 0 : 1;
+        DoLogging(lastMessage);
+        await Shell.Current.GoToAsync($"..?split={_split.Filename}");
+    }
+
+    private void RefreshProperties() {
+        SplitCommand.NotifyCanExecuteChanged();
+        OnPropertyChanged(nameof(Source));
+    }
+}
